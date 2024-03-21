114793cdb4a0d4d9a2b0dcec2d4b2d48fc9bd967
maxefimov <efimov.top.academy@gmail.com>
2024-03-21 20:48:12 +0300

    Добавьте файлы проекта.

---
+++ WpfLayoutApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLayoutApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Button btn = new();
+            //btn.Height = 50;
+            //btn.Background = new SolidColorBrush(Colors.Aqua);
+            //btn.Content = "Button 3";
+            //btn.Margin = new Thickness(20, 10, 20, 0);
+
+            //stack.Children.Add(btn);
+        }
+    }
+}
